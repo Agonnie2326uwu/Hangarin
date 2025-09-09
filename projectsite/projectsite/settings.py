588755db36032cb62c83894c4d990ffd855b4420
Agonnie2326uwu <carldecastro989@gmail.com>
2025-09-09 17:12:38 +0800

    modify allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5$qndf3(+!1#9&3v2$sqc7f8vx(ew1l2r_-#bu2vkq7)vkuce+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['HANGARIN.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
